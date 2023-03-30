92f79bfd86a84bf75acb9070cee54dc8a34d1feb
Stephen M. Walker II <stephen@innov.at>
2023-03-30 23:41:09 +0100

    test

--- wolverine_test.py
+++ wolverine_test.py
@@ -1,17 +1,10 @@
 import unittest
-from unittest.mock import patch, MagicMock
-import sys
-import subprocess
 import os
 import shutil
-import json
-import difflib
-import openai
+from unittest.mock import patch, MagicMock
 from wolverine import run_script, send_error_to_gpt4, apply_changes, main
 
-
 class TestWolverine(unittest.TestCase):
-
     def setUp(self):
         self.script_name = "wolverine_test.py"
         self.backup_file = self.script_name + ".bak"
@@ -21,61 +14,69 @@ class TestWolverine(unittest.TestCase):
             os.remove(self.backup_file)
 
     def test_run_script_success(self):
-        with patch("subprocess.check_output") as mock_check_output:
-            mock_check_output.return_value = b"Success"
-            output, returncode = run_script(self.script_name)
-            self.assertEqual(output, "Success")
-            self.assertEqual(returncode, 0)
+        with open(self.script_name, "w") as f:
+            f.write("print('Hello, World!')")
+
+        output, returncode = run_script(self.script_name)
+        self.assertEqual(output.strip(), "Hello, World!")
+        self.assertEqual(returncode, 0)
 
     def test_run_script_failure(self):
-        with patch("subprocess.check_output") as mock_check_output:
-            mock_check_output.side_effect = subprocess.CalledProcessError(
-                1, "cmd")
-            mock_check_output.return_value = b"Error"
-            output, returncode = run_script(self.script_name)
-            self.assertEqual(output, "Error")
-            self.assertEqual(returncode, 1)
+        with open(self.script_name, "w") as f:
+            f.write("print('Hello, World!'\n")
 
-    @patch("openai.ChatCompletion.create")
-    def test_send_error_to_gpt4(self, mock_chat_completion):
-        mock_chat_completion.return_value = MagicMock(
-            choices=[MagicMock(message=MagicMock(content="Suggested changes"))])
-        response = send_error_to_gpt4(
-            self.script_name, ["arg1", "arg2"], "Error message")
-        self.assertEqual(response, "Suggested changes")
+        output, returncode = run_script(self.script_name)
+        self.assertNotEqual(returncode, 0)
+
+    @patch("wolverine.openai.ChatCompletion.create")
+    def test_send_error_to_gpt4(self, mock_create):
+        mock_create.return_value = MagicMock(choices=[MagicMock(message=MagicMock(content='{"operation": "Replace", "line": 1, "content": "print(\\"Hello, World!\\")"}'))])
+
+        file_path = self.script_name
+        args = []
+        error_message = "SyntaxError: EOL while scanning string literal"
+
+        with open(file_path, "w") as f:
+            f.write("print('Hello, World!'\n")
+
+        changes_json = send_error_to_gpt4(file_path, args, error_message)
+        self.assertEqual(changes_json, '{"operation": "Replace", "line": 1, "content": "print(\\"Hello, World!\\")"}')
 
     def test_apply_changes(self):
-        changes_json = json.dumps([
-            {"operation": "Replace", "line": 1,
-                "content": "print('Hello, World!')"},
-            {"explanation": "Changed print statement"}
-        ])
         with open(self.script_name, "w") as f:
-            f.write("print('Hello')")
+            f.write("print('Hello, World!'\n")
+
+        changes_json = '[{"operation": "Replace", "line": 1, "content": "print(\\"Hello, World!\\")"}]'
         apply_changes(self.script_name, changes_json)
+
         with open(self.script_name, "r") as f:
             content = f.read()
-        self.assertEqual(content, "print('Hello, World!')\n")
+
+        self.assertEqual(content.strip(), 'print("Hello, World!")')
 
     @patch("wolverine.run_script")
     @patch("wolverine.send_error_to_gpt4")
     @patch("wolverine.apply_changes")
     def test_main(self, mock_apply_changes, mock_send_error_to_gpt4, mock_run_script):
-        mock_run_script.side_effect = [("Error", 1), ("Success", 0)]
-        mock_send_error_to_gpt4.return_value = json.dumps([
-            {"operation": "Replace", "line": 1,
-                "content": "print('Hello, World!')"},
-]
-)
-            {"explanation": "Changed print statement"}
-        self.assertTrue(os.path.exists(self.backup_file))
+        mock_run_script.side_effect = [("SyntaxError: EOL while scanning string literal", 1), ("Hello, World!", 0)]
+        mock_send_error_to_gpt4.return_value = '[{"operation": "Replace", "line": 1, "content": "print(\\"Hello, World!\\")"}]'
 
-    test_args = ['wolverine.py', self.script_name, 'arg1', 'arg2', '--revert']
-    with patch.object(sys, "argv", test_args):
+    def test_main_revert(self):
+        main()
+f.write("print('Hello, World!')")
+
+shutil.copy(self.script_name, self.backup_file)
+
+    with open(self.script_name, "w") as f:
+            f.write("print('Hello, World!'\n")
+
+        with patch("sys.argv", ["wolverine.py", self.script_name, "--revert"]):
             main()
 
-    self.assertTrue(os.path.exists(self.backup_file))
-    mock_apply_changes.assert_called_once()
+        with open(self.script_name, "r") as f:
+            content = f.read()
+
+        self.assertEqual(content.strip(), "print('Hello, World!')")
 
 if __name__ == "__main__":
     unittest.main()
