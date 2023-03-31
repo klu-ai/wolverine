1492ec8da4d90042d1d32af7df3b471acf215e54
Stephen M. Walker II <stephen@innov.at>
2023-03-31 08:35:49 +0100

    github

--- github.py
+++ github.py
@@ -5,9 +5,9 @@ from bs4 import BeautifulSoup
 def get_contribution_count(profile_url):
     response = requests.get(profile_url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    contribution_graph = soup.find('svg', class_='js-calendar-graph-svg')
-    contributions = [int(rect['data-count']) for rect in contribution_graph.find_all('rect')]
-    return sum(contributions)
+    contribution_text = soup.find('h2', class_='f4 text-normal mb-2').text.strip()
+    contribution_count = int(contribution_text.split()[0].replace(',', ''))
+    return contribution_count
 
 def get_most_active_coders(repo_url):
     response = requests.get(repo_url)
